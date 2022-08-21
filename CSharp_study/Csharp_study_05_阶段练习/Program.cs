74036aa598ebd263074c9195e90e4fa9ce99bb69
kala <kala@PICO>
2022-08-21 22:55:46 +0800

    20220821

---
+++ CSharp_study/Csharp_study_05_阶段练习/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Csharp_study_05_阶段练习
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+
+        }
+    }
+}
