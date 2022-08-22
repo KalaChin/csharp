ce744844852faedbef89e4f155f5fbc998bb7599
kala_work <Administrator@ZMZJ20220>
2022-08-22 18:05:58 +0800

    220822

--- CSharp_study/Csharp_study_05_阶段练习/Program.cs
+++ CSharp_study/Csharp_study_05_阶段练习/Program.cs
@@ -6,8 +6,42 @@ namespace Csharp_study_05_阶段练习
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
+            //练习1：用户输入一个三位的正整数，按数字相反的顺序输出
+            /*
+            Console.WriteLine("请输入一个三位的正整数！");
+            int threeNum = Convert.ToInt32(Console.ReadLine());
+            int ge = threeNum % 10;
+            int shi = threeNum / 10 % 10;
+            int bai = threeNum / 100;
+            Console.WriteLine("结果是：{0}{1}{2}",ge,shi,bai);
+            */
 
+            //练习2：B国每次发送的电报都是⼀个五位数组成的，这个五位数的⼗位和千位组成的两位数，就是对应
+            //      的电报内容，帮助B国对数据进⾏解密。
+            //
+            /*
+            Console.WriteLine("请输入需破译的电报：");
+            int dianbao = Convert.ToInt32(Console.ReadLine());
+            int fristNum = dianbao / 10000;
+            int secondNum = dianbao / 1000 % 10;
+            int thirdNum = dianbao / 100 % 10;
+            int fourthNum = dianbao / 10 % 10;
+            int fifthNum = dianbao % 10;
+            int he = (fourthNum * 10 + secondNum);
+            char neirong = (char)(he);
+            Console.WriteLine(he);
+            Console.WriteLine();
+            Console.WriteLine(neirong);
+            */
+
+            //练习3：输⼊数学的分数，和英语的分数，判断是否可以获得奖励，只有数学和英语分数都考到90分才有奖励。
+
+            Console.WriteLine("请输入数学成绩：");
+            int math = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("请输入语文成绩：");
+            int language = Convert.ToInt32(Console.ReadLine());
+            bool reward = math >= 90 && language >= 90;
+            Console.WriteLine(reward);
 
         }
     }
---
+++ CSharp_study/Csharp_study_06_条件语句/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Csharp_study_06_条件语句
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //if语句
+
+            /*
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age<18){
+                Console.WriteLine("不可以进入");
+            }
+            else
+            {
+                Console.WriteLine("可以进入啦");
+            }
+            */
+
+            //小练习
+            //商场举办活动，年龄在18——30岁才能参加，如果年龄是奇数那么就中奖了。
+            Console.WriteLine("请输入您的年龄！");
+            int age = Convert.ToInt32(Console.ReadLine());
+            int agetest = age % 2;
+            if (age>=18 && age <= 30)
+            {
+                if (agetest == 1)
+                {
+                    Console.WriteLine("恭喜您，中奖了！");
+                }
+                else {
+                    Console.WriteLine("很遗憾，您没有中奖！");
+                }
+            }
+            else {
+                Console.WriteLine("您不在活动范围内，不能参加");
+            }
+
+
+
+
+        }
+    }
+}
